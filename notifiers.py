35ea5f63afa857cb23b75632eb4fd00b4a70a852
forgithubb <130647039+forgithubb@users.noreply.github.com>
2023-04-13 08:26:49 -0400

    Update notifiers.py

--- notifiers.py
+++ notifiers.py
@@ -79,13 +79,9 @@ def send_telegram_message(message: str):
     #if public_expls_msg:
         #message = message + "\n" + public_expls_msg
 
-    #message = message.replace(".", "\.").replace("-", "\-").replace("(", "\(").replace(")", "\)").replace("_", "").replace("[","\[").replace("]","\]").replace("{","\{").replace("}","\}").replace("=","\=").replace("#", "\#").replace("*", "\*").replace("@", "\@").replace("&", "\&").replace(",", "\,")
-    #r = requests.get(f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage?parse_mode=MarkdownV2&text={message}&chat_id={telegram_chat_id}')
+    message = message.replace(".", "\.").replace("-", "\-").replace("(", "\(").replace(")", "\)").replace("_", "").replace("[","\[").replace("]","\]").replace("{","\{").replace("}","\}").replace("=","\=").replace("#", "\#").replace("*", "\*").replace("@", "\@").replace("&", "\&")
+    r = requests.get(f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage?parse_mode=MarkdownV2&text={message}&chat_id={telegram_chat_id}')
     
-    #without markdown
-    message = message.replace("-", "\-")
-    r = requests.get(f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage?text={message}&chat_id={telegram_chat_id}')
-
 
     resp = r.json()
     if not resp['ok']:
